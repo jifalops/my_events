ae2db209713b7a70880aeb19500d6f57f856e247
Jacob Phillips <jifalops@gmail.com>
2020-02-05 22:44:42 -0500

    Add helpers for working with the rtdb.

---
+++ lib/firebase_helpers/database.dart
@@ -0,0 +1,93 @@
+abstract class _BasicOperations {
+  /// Read the data at [path] once.
+  Future<Map<String, dynamic>> read(String path);
+
+  /// Do a partial update instead of overwriting an entire location.
+  Future<void> update(String path, Map<String, dynamic> data);
+
+  /// Remove the data at [path].
+  Future<void> delete(String path);
+
+  /// Create or overwrite.
+  Future<void> write(String path, Map<String, dynamic> data);
+}
+
+/// An abstraction of both Firestore and Firebase's real-time database.
+abstract class Database implements _BasicOperations {
+  const Database();
+
+  /// The underlying database e.g. Firestore or FirebaseDatabase.
+  Object get db;
+
+  Future<bool> exists(String path) async => (await read(path)) != null;
+
+  /// Create data where none exists.
+  Future<void> create(String path, Map<String, dynamic> data);
+
+  /// Be notified and react to data changes.
+  Stream<Map<String, dynamic>> stream(String path);
+
+  // /// Run a transaction. All reads must be performed before any writes.
+  // Future<void> transact(TransactionHandler handler,
+  //     {Duration timeout = const Duration(seconds: 5)});
+
+  /// Write to various locations atomically (offline friendly).
+  // Future<void> batchWrite(BatchHandler handler);
+
+  /// Generate a new ID or key to be used at [path].
+  String generateId(String path);
+
+  /// A value that will be converted into the server's timestamp when written.
+  dynamic get serverTimestamp;
+
+  static DateTime parseTimestamp(dynamic timestamp) {
+    return timestamp == null
+        ? null
+        : timestamp is int
+            ? DateTime.fromMillisecondsSinceEpoch(timestamp)
+            : timestamp is String ? DateTime.tryParse(timestamp) : null;
+  }
+
+  static Map<String, DateTime> parseTimestamps(Map timestamps) =>
+      timestamps?.map((k, v) => MapEntry(k as String, parseTimestamp(v)));
+
+  /// Remove any leading, trailing or repeated slashes.
+  static String normalize(String path) {
+    if (path.startsWith(pathSep)) path = path.substring(1);
+    if (path.endsWith(pathSep)) path = path.substring(0, path.length - 1);
+    return path.replaceAll(_multiSep, pathSep);
+  }
+
+  static final _multiSep = RegExp('$pathSep+');
+
+  /// Split a path into parts, after calling [Database.normalize(path)].
+  static List<String> split(String path) => normalize(path).split(pathSep);
+
+  static String idOf(String path) =>
+      path.substring(path.lastIndexOf(pathSep) + 1);
+  static String parentOf(String path) =>
+      path.substring(0, path.lastIndexOf(pathSep));
+
+  static String pathSep = '/';
+}
+
+/// Used by Firestore only.
+/// All reads must be done before any writes.
+abstract class Transaction implements _BasicOperations {}
+
+/// Used by Firestore only.
+abstract class WriteBatch {
+  Future<void> commit();
+
+  /// Do a partial update instead of overwriting an entire location.
+  void update(String path, Map<String, dynamic> data);
+
+  /// Remove the data at [path].
+  void delete(String path);
+
+  /// Create or overwrite.
+  void write(String path, Map<String, dynamic> data, {bool merge = false});
+}
+
+typedef TransactionHandler = Future<void> Function(Transaction);
+typedef BatchHandler = Future<void> Function(WriteBatch);
---
+++ lib/firebase_helpers/realtime_database.dart
@@ -0,0 +1,129 @@
+import 'package:firebase_database/firebase_database.dart' as rt;
+import 'database.dart';
+
+class RealtimeDatabase extends Database {
+  const RealtimeDatabase();
+
+  @override
+  rt.DatabaseReference get db => rt.FirebaseDatabase.instance.reference();
+
+  @override
+  Stream<Map<String, dynamic>> stream(String path) {
+    return db
+        .child(path)
+        .onValue
+        .map((event) => _castMapDeep(event.snapshot.value));
+  }
+
+  Stream<dynamic> streamValue(String path) {
+    return db.child(path).onValue.map((event) => event.snapshot.value);
+  }
+
+  @override
+  Future<Map<String, dynamic>> read(String path) async =>
+      _castMapDeep((await db.child(path).once()).value);
+
+  Future<dynamic> readValue(String path) async =>
+      (await db.child(path).once()).value;
+
+  /// Altough this is a simpler alternative to [batchWrite()], this should only
+  /// be used to modify a single location so that differences in Firestore and
+  /// the real-time database can be abstracted away.
+  @override
+  Future<void> update(String path, Map<String, dynamic> valuesToUpdate) =>
+      db.child(path).update(valuesToUpdate);
+
+  Future<void> create(String path, Map<String, dynamic> data) async {
+    transact(path, (existing) async {
+      return existing == null ? data : null;
+    });
+  }
+
+  Future<void> createValue(String path, dynamic value) async {
+    transact(path, (existing) async {
+      return existing == null ? value : null;
+    });
+  }
+
+  @override
+  Future<void> delete(String path) => db.child(path).remove();
+
+  @override
+  String generateId([String path = '']) => db.child(path).push().key;
+
+  @override
+  get serverTimestamp => rt.ServerValue.timestamp;
+
+  /// [handler] may be called multiple times, including with `null` data.
+  Future<Map<String, dynamic>> transact(String path,
+      Future<Map<String, dynamic>> Function(Map<String, dynamic>) handler,
+      {Duration timeout = const Duration(seconds: 5)}) async {
+    final result = await db.child(path).runTransaction((data) async {
+      final newData = await handler(_castMapDeep(data.value));
+      data.value = newData;
+      return data;
+    }, timeout: timeout);
+    if (result.error != null) {
+      throw result.error;
+    } else {
+      if (result.committed == false) {
+        // throw Exception('Transaction failed');
+      }
+      return _castMapDeep(result.dataSnapshot.value);
+    }
+  }
+
+  /// [handler] may be called multiple times, including with `null` data.
+  Future<dynamic> transactValue(
+      String path, Future<dynamic> Function(dynamic) handler,
+      {Duration timeout = const Duration(seconds: 5)}) async {
+    final result = await db.child(path).runTransaction((data) async {
+      final newData = await handler(data.value);
+      data.value = newData;
+      return data;
+    }, timeout: timeout);
+    if (result.error != null) {
+      throw result.error;
+    } else {
+      if (result.committed == false) {
+        // throw Exception('Transaction failed');
+      }
+      return result.dataSnapshot.value;
+    }
+  }
+
+  @override
+  Future<void> write(String path, Map<String, dynamic> data) =>
+      db.child(path).set(data);
+
+  Future<void> writeValue(String path, dynamic value) =>
+      db.child(path).set(value);
+
+  Future<bool> isConnected() => readValue('.info/connected');
+
+  Stream<bool> onConnectionChanged() => streamValue('.info/connected');
+
+  Future<void> writeOnDisconnect(String path, dynamic value) =>
+      db.child(path).onDisconnect().set(value);
+  Future<void> updateOnDisconnect(String path, Map<String, dynamic> value) =>
+      db.child(path).onDisconnect().update(value);
+  Future<void> removeOnDisconnect(String path) =>
+      db.child(path).onDisconnect().remove();
+  Future<void> cancelDisconnectListener(String path) =>
+      db.child(path).onDisconnect().cancel();
+
+  /// Add this value to the local device's time to get the server time (estimated).
+  ///
+  /// This can be affected by network latency and is mainly for discovering large
+  /// (> 1 second) discrepancies.
+  /// See https://firebase.google.com/docs/database/web/offline-capabilities#clock-skew
+  Future<int> clockSkew() async => int.tryParse(
+      (await readValue('.info/serverTimeOffset'))?.toString() ?? '0');
+
+  /// See [clockSkew()].
+  Stream<int> onClockSkewChanged() => streamValue('.info/serverTimeOffset')
+      .map((offset) => int.tryParse('${offset ?? 0}'));
+}
+
+Map<String, dynamic> _castMapDeep(Map value) =>
+    value?.map((k, v) => MapEntry('$k', v is Map ? _castMapDeep(v) : v));
