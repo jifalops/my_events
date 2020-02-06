416f4ee2cbaf7adccc57e51438aaed2464aecf06
Jacob Phillips <jifalops@gmail.com>
2020-02-05 21:32:18 -0500

    Add event form to home screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:my_events/controllers/auth_controller.dart';
@@ -31,6 +33,8 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(title: Text(appName)),
       body: ListView(
         children: <Widget>[
+          SizedBox(height: 48),
+
           /// Dart 2.4ish added a few "UI as code" features, one of which is to
           /// allow `if` and `else` inside of list literals (`[]`). Before this,
           /// an inline-if thing called a ternary was required.
@@ -50,11 +54,73 @@ class HomePage extends StatelessWidget {
                 FlatButton(
                   child: Text('Sign out'),
                   onPressed: signOut,
-                )
+                ),
+                Container(
+                  padding: EdgeInsets.all(48),
+                  child: EventForm(),
+                ),
               ],
             ),
         ],
       ),
+    );
+  }
+}
+
+class EventForm extends StatefulWidget {
+  @override
+  _EventFormState createState() => _EventFormState();
+}
+
+class _EventFormState extends State<EventForm> {
+  final title = TextEditingController();
+  /// Since this field isn't final, the `EventForm` widget is stateful.
+  DateTime start;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TextField(
+          controller: title,
+          decoration: InputDecoration(labelText: 'Event title'),
+        ),
+        DateTimeField(
+          decoration: InputDecoration(labelText: 'Start time'),
+          format: DateFormat("EEE. MMM d, yyyy 'at' h:mma"),
+          onShowPicker: (context, currentValue) async {
+            final date = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(2000),
+                lastDate: DateTime(2100));
+            if (date != null) {
+              final time = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              if (time != null) {
+                return DateTimeField.combine(date, time);
+              }
+            }
+            return currentValue;
+          },
+          onChanged: (value) => start = value,
+        ),
+        SizedBox(height: 24),
+        RaisedButton(
+          child: Text('Create event'),
+          onPressed: () {
+            if (title.text.isNotEmpty && start != null) {
+              print('Creating "${title.text}" at $start');
+              // TODO
+            } else {
+              Scaffold.of(context)
+                  .showSnackBar(SnackBar(content: Text('Invalid event')));
+            }
+          },
+        )
+      ],
     );
   }
 }
